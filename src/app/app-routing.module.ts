760741f81c35a515140b83edf6685dcc47beef5e
Черноморова <chernomorovaln@it2g.ru>
2020-12-23 22:27:42 +0300

    add main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'violet', component: HeroesComponent },
-  { path: 'rose', component: HeroesComponent }
+  { path: 'violet', component: MainComponent },
+  { path: 'rose', component: MainComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
@@ -18,18 +15,11 @@ import { HeroesComponent } from './heroes/heroes.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeroesComponent,
+    MainComponent,
   ],
   bootstrap: [ AppComponent ]
 })
